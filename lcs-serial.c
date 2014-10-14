6c12f84cac06b5fb9d114f55a5c0d449b61b8571
diogoreispinto <diogo.reis.pinto@gmail.com>
2014-10-14 11:34:44 +0100

    initial version of lcs problem - serial version

---
+++ lcs-serial.c
@@ -0,0 +1,128 @@
+/* Dynamic Programming implementation of LCS problem */
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <math.h>
+  
+int max(int a, int b);
+short cost(int x);
+void fillMatrixWithValues(int** mat,char* stringA,char* stringB,int m,int n);
+void computeLCSResult(int** mat,char* stringA,char* stringB,int m,int n);
+
+
+
+/* Computes de LCS and prints its length and value to the output */
+void lcs( char *stringA, char *stringB, int m, int n )
+{
+   int **mat = (int **)malloc(m * sizeof(int*));
+   int i=0;
+   for(i = 0; i < n; i++) 
+      mat[i] = (int *)malloc(n * sizeof(int));
+   fillMatrixWithValues(mat,stringA,stringB,m,n);
+   computeLCSResult(mat,stringA,stringB,m,n); 
+}
+
+
+
+/*Fills the matrix with the matching pattern then used to compute the LCS*/
+void fillMatrixWithValues(int** mat,char* stringA,char* stringB, int lenA, int lenB){
+   int i=0;
+   int j=0;
+   for (i=0; i<=lenA; i++)
+   {
+     for (j=0; j<=lenB; j++)
+     {
+       if (i == 0 || j == 0)
+         mat[i][j] = 0;
+       else if (stringA[i-1] == stringB[j-1]){
+          mat[i][j] = mat[i-1][j-1] + 1;
+          cost(i); 
+        }
+       else
+         mat[i][j] = max(mat[i-1][j], mat[i][j-1]);
+     }
+   }
+}
+
+
+
+/*Computes the LCS with the given matrix filled with the matching pattern*/
+void computeLCSResult(int** mat,char* stringA,char* stringB, int lenA, int lenB){
+   int index = mat[lenA][lenB];
+   char* lcs =(char*)malloc(sizeof(char)*(index+1)) ;
+   lcs[index] = '\0'; 
+   int i = lenA; 
+   int j = lenB;
+   while (i > 0 && j > 0)
+   {
+      if (stringA[i-1] == stringB[j-1])
+      {
+          lcs[index-1] = stringA[i-1]; 
+          i--; j--; index--;    
+      }
+      else if (mat[i-1][j] > mat[i][j-1])
+         i--;
+      else
+         j--;
+   }
+ 
+   int lcsSize = strlen(lcs);
+   // Print the size
+   printf("%d\n",lcsSize);
+
+   // Print the lcs
+   printf("%s",lcs);
+}
+
+
+
+short cost(int x) {
+	int i, n_iter = 20;
+	double dcost = 0;
+	for(i = 0; i < n_iter; i++)
+		dcost += pow(sin((double) x),2) + pow(cos((double) x),2);
+	return (short) (dcost / n_iter + 0.1);
+}
+
+
+
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+  
+
+
+int main(int argc, char *argv[])
+{
+  if(argc!=2)
+    return 0;
+ 
+  FILE *file = fopen(argv[1], "r" );
+
+  if ( file == 0 )
+  {
+      printf( "Could not open file\n" );
+  }
+  else 
+  {
+    /*Vai buscar a primeira linha com dois ints */
+    int A=0;
+    int B=0;
+    char sizes[1000];   
+    fgets(sizes,1000,file);
+    sscanf(sizes,"%d %d\n",&A,&B);
+    /*A e B guardam os valores de lenA e lenB. Ha algum problema porque eles mudam de valores nas linhas seguintes*/
+    int lenA = A;
+    int lenB = B;
+    char stringA[A+1];
+    char stringB[B+1];
+    char barraN[1];
+    fgets(stringA,A+1,file);
+    fgets(barraN,2,file);
+    fgets(stringB,B+1,file); 
+    fclose( file );
+    lcs( stringA, stringB, lenA, lenB );
+  }
+  return 0;
+}
