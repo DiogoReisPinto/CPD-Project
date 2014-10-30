f171e5eee929e2f8b69bd193784f6f8c9270a753
andrebispo5 <andrefsbispo@hotmail.com>
2014-10-30 22:57:50 +0000

    added the new parallel algorithm, based on an auxiliar matrix to compute the score matrix

--- lcs-omp.c
+++ lcs-omp.c
@@ -1,4 +1,4 @@
-/* Dynamic Programming implementation of LCS problem */
+/* Dynamic Programming LCS problem */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,7 +10,9 @@
   
 int max(int a, int b);
 short cost(int x);
-void fillMatrixWithValues(int** mat,char* stringA,char* stringB,int m,int n);
+void createP(int **p, char *c, char* stringB, int n);
+int getC(char c);
+void fillMatrixWithValues(int** mat,int **p,char* stringA,char* stringB,int m,int n);
 void computeLCSResult(int** mat,char* stringA,char* stringB,int m,int n);
 
 
@@ -18,54 +20,92 @@ void computeLCSResult(int** mat,char* stringA,char* stringB,int m,int n);
 
 /* Computes de LCS and prints its length and value to the output */
 void lcs( char *stringA, char *stringB, int m, int n ){
-   int **mat = (int **) malloc(sizeof(int *)* (m + 1));
-   int i=0;
-   #pragma omp parallel for
-   	for(i = 0; i <= m; i++)
-      mat[i] = (int *) malloc(sizeof(int)* (n + 1));
-   		fillMatrixWithValues(mat,stringA,stringB,m,n);
-   		computeLCSResult(mat,stringA,stringB,m,n);
+	int **mat = (int **) malloc(sizeof(int *)* (m + 1));
+	int **p = (int**) malloc(sizeof(int *)*27);
+	int i=0;
+	char *c = (char *) malloc(sizeof(char)*27);
+	c[0] = ' ';	
+   	#pragma omp parallel
+   	{ 
+		#pragma omp for nowait
+		for(i=1;i<=26;i++){
+			c[i]=(char)'A'+i-1;
+		}
+		
+   		#pragma omp for nowait
+   		for(i = 0; i <= m; i++) {
+      		mat[i] = (int *) malloc(sizeof(int)* (n + 1));		
+			mat[i][0] = 0;
+		}
 
-   	#pragma omp parallel for
-   	for(i = 0; i <= m; i++)
-      free(mat[i]);
-  free(mat);
+		#pragma omp for nowait
+   		for(i = 0; i <= n; i++) 
+			mat[0][i] = 0;
+	
+		
+		#pragma omp for
+		for (i = 0; i <= 26; i++) 
+			p[i] = (int *) malloc(sizeof(int)*(n + 1));
+	}
+
+	createP(p,c,stringB,n);
+	fillMatrixWithValues(mat,p,stringA,stringB,m,n);
+   	computeLCSResult(mat,stringA,stringB,m,n);
+
+   	#pragma omp parallel
+   	{
+   		#pragma omp for nowait
+   		for(i = 0; i <= m; i++)
+      		free(mat[i]);
+  		
+  		#pragma omp for
+		for(i = 0; i <= 26; i++)	
+			free(p[i]);		
+	}
+	free(p);
+	free(c);
+	free(mat);
 }
 
+//computes the P[i,j] matrix
+void createP(int **p, char *c, char* stringB, int n) {
+	int i, j;
 
+	#pragma omp parallel for private(j)  
+	for(i = 0; i <= 26; i++) {
+		for(j = 0; j <= n; j++) {
+			//eq. 6
+			if(j == 0)
+				p[i][j] = 0;
+			else if(stringB[j - 1] == c[i])
+				p[i][j] = j;
+			else p[i][j] = p[i][j - 1];
+			//
+		}
+	}
+}
+
+// gets the number of a char c on the array containing the finite alphabet
+int getC(char c) {
+	return c-'A'+1;
+}
 
 /*Fills the matrix with the matching pattern then used to compute the LCS*/
-void fillMatrixWithValues(int** mat,char* stringA,char* stringB, int lenA, int lenB){
-   int diagonalSize=0;
-   int k=0;
-   int iterations = lenA+lenB;
-   int diagonalLimit = 1;
-   for(k=0;k<=iterations;k++){
-    #pragma omp parallel for
-      for (diagonalSize=0; diagonalSize < diagonalLimit; diagonalSize++){
-        int j=diagonalSize;
-        int i =diagonalLimit - 1 - diagonalSize;
-        if(i>lenA || j>lenB)
-          continue;
-        else if (i == 0 || j == 0)
-          mat[i][j] = 0;
-        else if (stringA[i-1] == stringB[j-1]){
-          mat[i][j] = mat[i-1][j-1] + 1;
-          cost(i); 
-        }
-        else
-          mat[i][j] = max(mat[i-1][j], mat[i][j-1]);
-      }
-      diagonalLimit++;   
-  }
-  
-  /*for(y=0;y<=lenB;y++){
-    for(x=0;x<=lenA;x++){
-    
-      printf("%d ",mat[x][y]);
-    }
-    printf("\n");
-  }*/
+void fillMatrixWithValues(int** mat, int **p,char* stringA,char* stringB, int lenA, int lenB){
+	int i, j, s, t;
+
+	for(i = 1; i <= lenA; i++) {
+		#pragma omp parallel for private(s,t)
+		for(j = 1; j <= lenB; j++) {
+			//eq. 7
+			t = (0 - p[getC(stringA[i - 1])][j]) < 0 ? 1 : 0;
+			s = (0 - (mat[i - 1][j]-t*mat[i - 1][p[getC(stringA[i - 1])][j]-1])) < 0 ? 1 : 0;
+			if(stringA[i-1]==stringB[j-1])
+				cost(i);
+			mat[i][j] = mat[i - 1][j] + t*(s ^ 1);
+			//
+		}
+	}
 }
 
 
@@ -100,7 +140,6 @@ void computeLCSResult(int** mat,char* stringA,char* stringB, int lenA, int lenB)
 }
 
 
-
 short cost(int x) {
 	int i, n_iter = 20;
 	double dcost = 0;
@@ -120,32 +159,25 @@ int max(int a, int b)
 
 int main(int argc, char *argv[])
 {
-  double start = omp_get_wtime();
-  if(argc!=2)
-    return 0;
+  	if(argc!=2)
+    	return 0;
 
-  FILE *file = fopen(argv[1], "r" );
+  	FILE *file = fopen(argv[1], "r" );
 
-  if ( file == 0 )
-  {
-      printf( "Could not open file\n" );
-  }
-  else 
-  {
-    int lenA=0;
-    int lenB=0;
-	 fscanf(file,"%d %d\n",&lenA,&lenB);
-	 char *stringA = (char *) malloc(sizeof(char)*(lenA+1));
-    char *stringB = (char *) malloc(sizeof(char)*(lenB+1));
-	 fscanf(file,"%s\n",stringA);
-	 fscanf(file,"%s\n",stringB);
-	 fclose( file );
-   lcs( stringA, stringB, lenA, lenB );
-	 free(stringA);
-    free(stringB);
-  }
-  double end = omp_get_wtime();
-  double processingTime = end-start;
-  printf("The processing time was %f\n", processingTime);
-  return 0;
+  	if ( file == 0 ){
+      	printf( "Could not open file\n" );
+  	}else{
+    	int lenA=0;
+    	int lenB=0;
+	 	fscanf(file,"%d %d\n",&lenA,&lenB);
+	 	char *stringA = (char *) malloc(sizeof(char)*(lenA+1));
+    	char *stringB = (char *) malloc(sizeof(char)*(lenB+1));
+	 	fscanf(file,"%s\n",stringA);
+	 	fscanf(file,"%s\n",stringB);
+	 	fclose( file );
+   		lcs( stringA, stringB, lenA, lenB );
+	 	free(stringA);
+    	free(stringB);
+  	}
+  	return 0;
 }
